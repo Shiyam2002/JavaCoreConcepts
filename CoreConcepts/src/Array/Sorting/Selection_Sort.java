1fedd168d1bd1221efa3e3cbc861c504e27bee8b
Shiyam.K.S <k.s.shiyam2002@gmail.com>
2023-07-30 21:06:57 +0530

    Updated selection sort

--- CoreConcepts/src/Array/Sorting/Selection_Sort.java
+++ CoreConcepts/src/Array/Sorting/Selection_Sort.java
@@ -6,18 +6,19 @@ public class Selection_Sort {
 
 	public static void main(String[] args) {
 		int[] arr = {4,7,1,8,3,5,9};
-		int min_ele ;
+		
 		
 		System.out.println(Arrays.toString(arr));
-		for(int i=0;i<arr.length;i++) {
-			min_ele = arr[i];
+		for(int i=0;i<arr.length-1;i++) {
+		    int min=i;
 			for(int j=i+1;j<arr.length;j++) {
-				if(min_ele > arr[j]) {
-					int temp = min_ele;
-					min_ele = arr[j];
-					arr[j] = temp;
+				if(arr[min]>arr[j]) {
+					min =j;
 				}
-			}
+				int temp = arr[min];
+				arr[min] = arr[i];
+				arr[i] = temp;
+			}	
 		}
 		
 		System.out.println(Arrays.toString(arr));
