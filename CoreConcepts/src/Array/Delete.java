45bbaf6fa16bf058b9b0f4ed2fc23c1fb4f50940
Shiyam.K.S <k.s.shiyam2002@gmail.com>
2023-08-18 17:15:30 +0530

    del

---
+++ CoreConcepts/src/Array/Delete.java
@@ -0,0 +1,22 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Delete {
+
+	public static void main(String[] args) {
+		int[] age = {23,54,25,76,34};
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Which element do you want to delete ?");
+		int key = sc.nextInt();
+		
+		for(int i=0;i<age.length;i++) {
+			if(age[i] == key) {
+				
+			}
+		}
+
+	}
+
+}
