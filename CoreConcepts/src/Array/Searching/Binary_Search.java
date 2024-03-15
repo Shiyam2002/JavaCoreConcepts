c35a21cca911d364df8af1bb0191dcaf5f4c8c3c
Shiyam.K.S <k.s.shiyam2002@gmail.com>
2024-03-15 14:32:51 +0530

    Update Binary_Search.java

--- CoreConcepts/src/Array/Searching/Binary_Search.java
+++ CoreConcepts/src/Array/Searching/Binary_Search.java
@@ -2,25 +2,32 @@ package Array.Searching;
 
 public class Binary_Search {
 	
-	public void binary(int arr[],int key) {
-		int left=0;
-		int right = arr.length-1;
-		int mid = (left+right)/2;
-		for(int i=0;i<arr.length;i++) {
-			if(arr[mid] == key) {
-				System.out.println("Value found at the index "+mid);
-			}
-			else if(arr[mid]>key) {
-				
-			}
-		}
-	}
-	
 	public static void main(String[] args) {
-		int[] arr = { 2,4,6,8,10,12};
-		int key = 10;
-		Binary_Search find = new Binary_Search();
-		find.binary(arr,10);
+		int[] arr = {1,3,5,7,9,11,13};
+		int target =7;
+		
+		int result = binary(arr ,target);
+		if(result != -1) {
+			System.out.println("Element found at index : "+ result);
+		}
+		else
+			System.out.println("Element not found");
 	}
 
+	private static int binary(int[] arr, int target) {
+		int left = 0;
+		int right = arr.length-1;
+		int mid;
+		
+		for(int i=0;i<arr.length;i++) {
+			mid = (left+right)/2;
+			if(arr[mid] == target)
+				return mid;
+			else if(arr[mid] >target)
+				right = mid-1;
+			else
+				left = mid+1;
+		}
+		return -1;
+	}
 }
